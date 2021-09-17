2cb4dc073eb488e79cfc197f63bdbb6bd92e2c74
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-09-17 13:36:05 +0200

    basic crud and admin install

--- graphql/resolvers/orders.js
+++ graphql/resolvers/orders.js
@@ -1,14 +1,95 @@
+const { AuthenticationError, UserInputError } = require("apollo-server");
+
 const Order = require("../../models/order.model");
+const checkAuth = require("../../util/check-auth");
 
 module.exports = {
   Query: {
-    async Orders() {
+    async orders() {
       try {
-        const orders = await Order.find();
+        const orders = await Order.find().sort({ createdAt: -1 });
         return orders;
       } catch (err) {
-        throw new Error(`il y a une erreur:  ${err}`);
+        throw new Error(err);
       }
+    },
+    async order(_, { orderId }) {
+      try {
+        const order = await Order.findById(orderId);
+        if (order) {
+          return order;
+        } else {
+          throw new Error("Order not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createOrder(_, { body }, context) {
+      const user = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new Error("Order body must not be empty");
+      }
+
+      const newOrder = new Order({
+        body,
+        user: user.id,
+        email: user.email,
+        createdAt: new Date().toISOString(),
+      });
+
+      const order = await newOrder.save();
+
+      // context.pubsub.publish("NEW_POST", {
+      //   newOrder: order,
+      // });
+
+      return order;
+    },
+    async deleteOrder(_, { orderId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const order = await Order.findById(orderId);
+        if (user.email === order.email) {
+          await order.delete();
+          return "Order deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    async likeOrder(_, { orderId }, context) {
+      const { email } = checkAuth(context);
+
+      const order = await Order.findById(orderId);
+      if (order) {
+        if (order.likes.find((like) => like.email === email)) {
+          // Order already likes, unlike it
+          order.likes = order.likes.filter(
+            (like) => like.email !== email
+          );
+        } else {
+          // Not liked, like order
+          order.likes.push({
+            email,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await order.save();
+        return order;
+      } else throw new UserInputError("Order not found");
+    },
+  },
+  Subscription: {
+    newOrder: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator("NEW_POST"),
     },
   },
 };
--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,49 +1,119 @@
-require("dotenv").config({ path: "./config/.env" });
-const User = require("../../models/user.model");
+const { UserInputError } = require("apollo-server");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
+require("dotenv").config({ path: "./config/.env" });
 const SECRET_KEY = process.env["SECRET_KEY"];
+const User = require("../../models/user.model");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+
+// TOKEN GENERATOR FUNCTION
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 
 module.exports = {
-  Mutation: {
-    async register(
-      parent,
-      { registerInput: { password, confirmPassword, email } },
-      context,
-      info
-    ) {
-      password = await bcrypt.hash(password, 10);
-      const newUser = new User({
-        email,
-        password,
-        createdAt: new Date().toISOString(),
-      });
-
-      const res = await newUser.save();
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
-      return {
-        ...res._doc,
-        id: res._id,
-        token,
-      };
-    },
-  },
-
   Query: {
-    async Users() {
+    async users() {
       try {
         const users = await User.find();
         return users;
       } catch (err) {
         throw new Error(`il y a une erreur:  ${err}`);
       }
+    },
+    async user(_, { userId }) {
+      try {
+        const user = await User.findById(userId);
+        if (user) {
+          return user;
+        } else {
+          throw new Error("User not found" + userId);
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    //LOGIN
+    async login(_, { email, password }) {
+      const { errors, valid } = validateLoginInput(email, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong crendetials";
+        throw new UserInputError("Wrong crendetials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
+    //REGISTER
+    async register(_, { registerInput: { email, password, confirmPassword } }) {
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      // TODO: Make sure user doesnt already exist
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new UserInputError("Email already exist", {
+          errors: {
+            email: "This email already exist",
+          },
+        });
+      }
+      // hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        password,
+      });
+
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
