6e3f17029d641dd47290edd607396c6773d528d5
Tanguy Clarence <tanguy.clarence@gmail.com>
2021-09-19 12:43:40 +0200

    defined port

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ const { ApolloServer } = require("apollo-server");
 const mongoDb = require("./config/db.js");
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
+require("dotenv").config({ path: "./config/.env" });
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const port = process.env["PORT"] || 80;
+
 //Connect DB
 mongoDb
   .then(() => {
     console.log("connect to the database");
-    server.listen({ port: 3000 });
+    server.listen({ port: port });
   })
   .then(() => {
-    console.log(`Server is running at 3000`);
+    console.log(`Server is running at ${port}`);
   })
   .catch((err) => console.log("failed to connect:" + err));
